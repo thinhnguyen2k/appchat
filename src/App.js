0a6ba7ea6b4c0eedc45aad47224fa487258e95ee
Thinh Nguyen <insecprolife11@gmail.com>
2022-03-13 20:13:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+    return(
+        <ChatEngine
+            height = "100vh"
+            projectID = "3a3e9721-6b76-4798-9f4e-08c35010fae9"
+            userName = "MedicalBooking"
+            userSecret = "123456"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {... chatAppProps}/>}
+        />
+    )
 }
 
 export default App;
